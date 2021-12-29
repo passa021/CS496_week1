21f3cffdb13a4cea0f527c2a81167f4a2c460f18
minchaekim <passa021@korea.ac.kr>
2021-12-29 16:55:33 +0900

    gallery

---
+++ app/src/main/java/com/example/cs496_week1/ImageAdapter.java
@@ -0,0 +1,59 @@
+package com.example.cs496_week1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+//import android.widget.GridView;
+import android.widget.ImageView;
+
+
+public class ImageAdapter extends BaseAdapter {
+    Context context = null;
+
+    int[] imageIDs = null;
+
+    public ImageAdapter(Context context, int[] imageIDs){
+        this.context = context;
+        this.imageIDs = imageIDs;
+    }
+
+    public int getCount() {
+        return (null != imageIDs) ? imageIDs.length : 0;
+    }
+
+    public Object getItem(int position) {
+        return (null != imageIDs) ? imageIDs[position] : 0;
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ImageView imageView;
+
+        if(null != convertView){
+            imageView = (ImageView) convertView;
+        }
+        else {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 128;
+            Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), imageIDs[position],options);
+            bmp = Bitmap.createScaledBitmap(bmp, 100, 100, true);
+
+            imageView = new ImageView(context);
+            imageView.setAdjustViewBounds(true);
+            imageView.setImageBitmap(bmp);
+
+            ImageClickListener imageViewClickListener = new ImageClickListener(context, imageIDs[position]);
+            imageView.setOnClickListener(imageViewClickListener);
+        }
+
+        return imageView;
+    }
+}
+
+
---
+++ app/src/main/java/com/example/cs496_week1/ImageClickListener.java
@@ -0,0 +1,24 @@
+package com.example.cs496_week1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ImageClickListener implements OnClickListener {
+
+    Context context;
+
+    int imageID;
+
+    public ImageClickListener(Context context, int imageID) {
+        this.context = context;
+        this.imageID = imageID;
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(context, ImageActivity.class);
+        intent.putExtra("image ID", imageID);
+        context.startActivity(intent);
+    }
+}
